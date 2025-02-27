227f60d882d69ea15db7a67286cceef9d4e60f4f
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-02-27 06:21:07 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class odd{
+public static void main(String[] args){
+Scanner Sc = new Scanner(System.in);
+int i = Sc.nextInt();
+if(i % 2==0){
+System.out.println("Even");
+}
+else{
+System.out.println("Odd");
+}
+}
+}
+
