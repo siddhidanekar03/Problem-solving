fe192b6a38d2da911efb48ec534460131d4af8b5
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-05-06 22:00:52 +0530

    Add files via upload

---
+++ knapsack.java
@@ -0,0 +1,28 @@
+public class knapsack {
+    public static int knapsackTab(int val[], int wt[], int W, int n) {
+        int dp[][] = new int[n + 1][W + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (wt[i - 1] <= j) {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] val = {60, 100, 120};
+        int[] wt = {10, 20, 30};
+        int W = 50;
+        int n = val.length;
+
+        int maxValue = knapsackTab(val, wt, W, n);
+        System.out.println("Maximum value in knapsack: " + maxValue);
+    }
+}
