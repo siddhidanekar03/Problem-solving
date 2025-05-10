5ce333086792570146a3863d6c9d18b39d193b7c
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-05-10 10:24:16 +0530

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float celsius,fahrenheit;
+printf("enter the temperature in degree celsius");
+scanf("%f",&celsius);
+fahrenheit=1.8*celsius+32;
+printf("temperature in fahrenheit is%f",fahrenheit);
+return 0;	
+}
