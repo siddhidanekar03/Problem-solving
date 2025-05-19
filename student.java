8226c7b53c2ca291bc7e1d027087183e98f5d626
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-05-19 21:06:06 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,26 @@
+class student
+
+{
+int roll_no;
+String name;
+
+public static void main(String args[])
+
+{
+
+student s1=new student();
+s1.roll_no =77;
+s1.name= "TEJAM BHAGT SING ";
+
+student s2=new student();
+
+s2.roll_no=78;
+s2.name= "RAMA";
+
+System.out.println(s1.roll_no+"\n"+s1.name+"\n"+s2.name+"\n"+s2.roll_no);
+
+}
+}
+
+
+
