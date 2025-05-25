eafabeaeb705a12135fadb945edc6d42f9c34adc
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-05-25 22:47:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example inputs
+        String a = "1010";
+        String b = "1011";
+
+        String result = solution.addBinary(a, b);
+
+        System.out.println("Binary sum of " + a + " and " + b + " is: " + result);
+    }
+}
