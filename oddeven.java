890dc96c33e2b2e036a8c6720c6ea28e62407947
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-06-01 22:49:30 +0530

    Add files via upload

--- oddeven.java
+++ oddeven.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class odd{
-public static void main(String[] args){
-Scanner Sc = new Scanner(System.in);
-int i = Sc.nextInt();
-if(i % 2==0){
-System.out.println("Even");
-}
-else{
-System.out.println("Odd");
-}
-}
-}
+public class oddeven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Ask the user to input a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        // Close the scanner object to avoid memory leaks
+        scanner.close();
+    }
+}
